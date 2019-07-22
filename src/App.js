3a4ce247e51c88edd27483552f7feb2d182a6ec8
IFNET-FOR\ricardo.meira <ricardo.meira@ifactory.com.br>
2019-07-22 11:36:36 -0300

    Lecture 152

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Layout from './components/Layout/Layout';
+import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
+
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Layout>
+        <BurguerBuilder></BurguerBuilder>
+      </Layout>
     </div>
   );
 }
---
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Auxiliary from '../../hoc/Auxiliary'
+
+class BurguerBuilder extends Component{
+    render () {
+        return (
+            <Auxiliary>
+                <div>Burguer</div>
+                <div>Build Controls</div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default BurguerBuilder;
