6eda48b1faac6acdd41be102c3c1c91b51fbc75c
IFNET-FOR\ricardo.meira <ricardo.meira@ifactory.com.br>
2019-07-24 13:41:21 -0300

    Lecture 160: Add ingredients

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -4,6 +4,13 @@ import Burguer from '../../components/Burguer/Burger';
 import BuildControls from '../../components/Burguer/BuildControls/BuildControls';
 import Auxiliary from '../../hoc/Auxiliary';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    chesse: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
+
 class BurguerBuilder extends Component{
     /*constructor(props) {
         super(props);
@@ -13,17 +20,38 @@ class BurguerBuilder extends Component{
     state = {
         ingredients: {
             salad: 0,
-            bacon: 2,
-            chesse: 0,
+            bacon: 0,
+            cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngridientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition
+
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    }
+
+    removeIngridientHandler = (type) => {
+
     }
 
     render () {
         return (
             <Auxiliary>
                 <Burguer ingredients={this.state.ingredients}></Burguer>
-                <BuildControls />
+                <BuildControls 
+                    ingredientAdded={this.addIngridientHandler}/>
             </Auxiliary>
         );
     }
