2ce50d30ca3f287ef241f1c1c1bad2b16fd24763
IFNET-FOR\ricardo.meira <ricardo.meira@ifactory.com.br>
2019-07-24 08:45:41 -0300

    Lecture 157: Burguer Dynamically

--- src/components/Burguer/Burger.js
+++ src/components/Burguer/Burger.js
@@ -4,11 +4,17 @@ import classes from './Burguer.module.css';
 import BurguerIngredient from './BurguerIngredient/BurguerIngredient';
 
 const burguer = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map((igKey) => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurguerIngredient key={igKey + i} type={igKey}/>
+            });
+        });
+
     return (
        <div className={classes.Burguer}>
             <BurguerIngredient type="bread-top"></BurguerIngredient>
-            <BurguerIngredient type="cheese"></BurguerIngredient>
-            <BurguerIngredient type="meat"></BurguerIngredient>
+            {transformedIngredients}
             <BurguerIngredient type="bread-bottom"></BurguerIngredient>
        </div> 
     );
--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -4,10 +4,24 @@ import Burguer from '../../components/Burguer/Burger'
 import Auxiliary from '../../hoc/Auxiliary'
 
 class BurguerBuilder extends Component{
+    /*constructor(props) {
+        super(props);
+        this.state = {...};
+    }*/
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            chesse: 2,
+            meat: 2
+        }
+    }
+
     render () {
         return (
             <Auxiliary>
-                <Burguer></Burguer>
+                <Burguer ingredients={this.state.ingredients}></Burguer>
                 <div>Build Controls</div>
             </Auxiliary>
         );
