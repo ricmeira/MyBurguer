ae46b4f4c4d1fc73ab43fa2e1c4f8f2bb6f157f0
IFNET-FOR\ricardo.meira <ricardo.meira@ifactory.com.br>
2019-07-30 17:00:28 -0300

    Lectures 177/178: SideDrawer Toggle with Icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,17 +7,23 @@ import classes from './Layout.module.css';
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     };
 
     sideDrawerClosedHandler = () => {
         this.setState({showSideDrawer: false});
     }
 
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+
     render() {
         return (
             <Auxiliary>
-                <Toolbar/>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
                 <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.showSideDrawer}/>
                 <main className={classes.Content}>
                     {this.props.children}
