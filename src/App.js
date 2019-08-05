532aeb537bdbb7584d9cfa27c15a187fc52bc009
Ricardo Meira <ricardogdmeira@gmail.com>
2019-08-05 13:43:10 -0300

    Lecture 212: Removind unused interceptors

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <BurguerBuilder></BurguerBuilder>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  state = {
+    show: true
+  }
+
+  /*componentDidMount() {
+    setTimeout(() => {
+      this.setState({show: false});
+    }, 5000)
+  }*/
+
+  render() {
+    return (
+      <div>
+        <Layout>
+          { this.state.show ?<BurguerBuilder /> : null }
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
