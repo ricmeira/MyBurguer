47450fa6a36a4cc27cfeda19cad4d8d01a5c1ad7
Ricardo Meira <ricardogdmeira@gmail.com>
2019-08-09 14:15:46 -0300

    Lecture 257: Fetching orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
-
+import axios from '../../axios-orders';
 import Order from '../../components/Order/Order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
+
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json').then(response => {
+            const fetchedOrders = [];
+            for(let key in response.data) {
+                fetchedOrders.push({...res.data[key], id: key});
+            }
+            this.setState({loading: false, orders: fetchedOrders})
+        }).catch(error => {
+            this.setState({loading: false})
+        });
+    }
+
     render () {
         return (
             <div>
@@ -12,4 +32,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
