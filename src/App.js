116d6d264df51a9b9fe66006b2a9089426ba04be
Ricardo Meira <ricardogdmeira@gmail.com>
2019-08-09 11:37:35 -0300

    Lecture 256: Creating orders page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   state = {
@@ -23,6 +24,7 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={BurguerBuilder} />
             <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
           </Switch>
         </Layout>
       </div>
