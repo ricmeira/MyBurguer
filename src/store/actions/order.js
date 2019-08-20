c9733a5b399ccb04d1b0eaef2c4d398605a507b7
Ricardo Meira <ricardogdmeira@gmail.com>
2019-08-20 10:27:44 -0300

    Lecture 335: Adding order actions

---
+++ src/store/actions/order.js
@@ -0,0 +1,27 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
+
+export const purchaseBurguerSuccess = (id, orderData) => {
+    return {
+        type: actionTypes.PURCHASE_BURGUER_SUCCESS,
+        orderId: id,
+        orderData, orderData
+    };
+};
+
+export const purchaseBurguerFail = (error) => {
+    return {
+        type: actionTypes.PURCHASE_BURGUER_FAIL,
+        error: error
+    };
+};
+
+export const purchaseBurguerStart = (orderData) => {
+    return dispatch => {
+        axios.post('/orders.json', order).then(response => {
+            dispatch(purchaseBurguerSuccess(response.data, orderData));
+        }).catch(error => {
+            dispatch(purchaseBurguerFail(error));
+        });
+    }
+}
