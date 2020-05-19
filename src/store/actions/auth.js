3d7e53324156395bb100130568e7b1da2c3ea90d
Ricardo <ricardogdmeira@gmail.com>
2020-05-19 12:08:50 -0300

    Lecture 415: ReduxSaga - Moving Logic from action creator to a Saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -2,11 +2,11 @@ import * as actionTypes from './actionTypes';
 import axios from 'axios';
 
 export const logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('expirationDate');
-    localStorage.removeItem('userId');
+    // localStorage.removeItem('token');
+    // localStorage.removeItem('expirationDate');
+    // localStorage.removeItem('userId');
     return {
-        type: actionTypes.AUTH_LOGOUT
+        type: actionTypes.AUTH_INITIATE_LOGOUT
     }
 }
 
