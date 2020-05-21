188796622ca083e92286252da04c5d7d4d7fd2c8
Ricardo <ricardogdmeira@gmail.com>
2020-05-21 15:52:38 -0300

    Lecture 419: ReduxSaga - Moving the BurgerBuilder side effects into a saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,10 +1,15 @@
 import { takeEvery } from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 import { logoutSaga, checkAuthTimeout, authUserSaga, authCheckStateSaga } from './auth';
+import { initIngredientsSaga } from './burgerBuilder';
 
 export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeout);
     yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
     yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
+}
+
+export function* watchBurgerBuilder() {
+    yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
 }
